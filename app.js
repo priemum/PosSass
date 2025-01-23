fa98d6b79609a7d8743184a6d28dd3bab62a4c6f
priemum <50713962+priemum@users.noreply.github.com>
2025-01-23 12:57:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(require("./routes"));
-
+ 
 // Routes
 app.use('/api/', sycn);
 const rateLimit = require('express-rate-limit');
